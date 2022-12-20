a7c047e18f0d6e38cb1a7e4d7845c2e4c4c8f8d2
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2022-12-20 10:31:11 -0300

    create first test

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('Should add item to cart', async ({ page }) => {
+    await page.goto('https://ecommerce-playground.lambdatest.io/');
+
+    await page.locator('span.title', { hasText: 'Mega Menu' }).hover();
+    await page.locator('a[title=Desktop]').click();
+
+    await page
+        .locator("div.carousel-item.active > img[title='HTC Touch HD']")
+        .click();
+    await page.locator("#container button[title='Add to Cart']").click();
+
+    await page
+        .locator('a.btn.btn-primary.btn-block', { hasText: 'View Cart' })
+        .click();
+    await expect(
+        page.locator('td.text-left', { hasText: 'HTC Touch HD' }),
+    ).toBeVisible();
+    await expect(page.locator("div[class$='flex-nowrap'] > input")).toHaveValue(
+        '1',
+    );
+});
