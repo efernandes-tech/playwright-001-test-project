e6d6e2eeb86bb43547b61412f6d53a526419117e
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2022-12-20 11:26:33 -0300

    run test using api

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -21,3 +21,24 @@ test('Should add item to cart', async ({ page }) => {
         '1',
     );
 });
+
+test('Should add item to cart using API', async ({ page }) => {
+    const Url = 'https://ecommerce-playground.lambdatest.io/index.php/';
+    await page.goto(Url);
+    const response = await page.request.post(Url, {
+        params: {
+            route: 'checkout/cart/add',
+        },
+        form: {
+            product_id: 28,
+            quantity: 1,
+        },
+    });
+    await page.goto(`${Url}?route=checkout/cart`);
+    await expect(
+        page.locator('td.text-left', { hasText: 'HTC Touch HD' }),
+    ).toBeVisible();
+    await expect(page.locator("div[class$='flex-nowrap'] > input")).toHaveValue(
+        '1',
+    );
+});
